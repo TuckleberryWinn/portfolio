2de2c4f3cb9ad1e879073a3524643139b1e1befd
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-01-19 22:21:13 -0700

    Start to build image upload UI

--- src/views/CMSView.vue
+++ src/views/CMSView.vue
@@ -1,45 +1,99 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { encode } from 'base64-arraybuffer';
+	import { computed, ref, watch } from 'vue';
+	const title = ref('');
+	const slug = ref('');
+	const thumbnail = ref('');
+	const thumbnailType = ref('');
+	const status = ref('');
+	const loading = ref(false);
+	watch(title, (newVal, oldVal) => {
+		slug.value = newVal.toLocaleLowerCase().replace(/\W/g, '_');
+	});
+	const handleSubmit = async () => {
+		status.value = 'Uploading to server...';
+		const data = {
+			title: title.value,
+			destination: slug.value,
+			slug: slug.value,
+			thumbnail: thumbnail.value,
+			thumbnailType: thumbnailType.value,
+		};
+		const responseObject = await fetch('/api/submit', {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+			method: 'POST',
+			mode: 'cors',
+		});
+		console.log('What is responseObject', responseObject);
+		const responseData = await responseObject.json();
+		console.log('What is responseData?', responseData);
+		status.value = 'Uploaded successfully!';
+		return responseData;
+	};
+	const handleThumbnailChange = async (changeEvent: InputEvent) => {
+		thumbnail.value = '';
+		status.value = 'Processing image';
+		const target = changeEvent.target as HTMLInputElement;
+		const file = (target.files as FileList)[0] as File;
+		thumbnailType.value = file.type;
+		console.log('What is file?', file.type);
+		const buffer = await file.arrayBuffer();
+		//console.log('what is changeEvent?', changeEvent);
+		//console.log('what is buffer?', buffer);
+		thumbnail.value = encode(buffer);
+		status.value = 'Image processed';
+	};
+	const isFormValid = computed(() => {
+		return thumbnail.value && title.value && slug.value;
+	});
+</script>
 
 <template>
 	<div>
-		<form>
-			<label for="projectTitle"
-				>Project Title:
+		<form @submit.prevent="handleSubmit">
+			<label>
+				<span>Project Title:</span>
 				<input
 					type="text"
-					id="projectTitle"
-					name="projectTitle"
+					v-model="title"
 					placeholder="Project Title"
 				/>
 			</label>
-			<label for="slugTitle"
-				>Slug Title:
+			<label>
+				<span>Slug Title:</span>
 				<input
 					type="text"
-					id="slugTitle"
-					name="slugTitle"
+					v-model="slug"
 					placeholder="project-title"
 				/>
 			</label>
-			<label for="coverImage">
-				Cover Image
+			<label>
+				<span>Cover Image:</span>
 				<input
 					type="file"
-					name="coverImage"
-					id="coverImage"
-					value="Submit"
+					@change="handleThumbnailChange"
+					accept=".png,.jpg,.gif,.webp"
 				/>
 			</label>
-			<input
-				class="inline"
-				type="reset"
-				value="Reset"
-			/>
-			<input
-				class="inline"
-				type="submit"
-				value="Submit"
-			/>
+			<div>
+				<code>{{ status }}</code>
+			</div>
+			<div>
+				<input
+					class="inline"
+					type="reset"
+					value="Reset"
+				/>
+				<input
+					class="inline"
+					type="submit"
+					value="Submit"
+					:disabled="!isFormValid || loading"
+				/>
+			</div>
 		</form>
 	</div>
 </template>
@@ -58,12 +112,16 @@
 	}
 	input {
 		display: block;
+		color: var(--pal-block-border);
 		background-color: var(--pal-block-bg);
 		border: 3px solid var(--pal-block-border);
 		border-radius: 4px;
 		margin: 0 1rem;
 		min-width: 6rem;
 		font-size: 1rem;
+	}
+	input:disabled {
+		opacity: 0.5;
 	}
 	input.inline {
 		display: inline-block;
@@ -72,5 +130,13 @@
 	input:hover {
 		background-color: var(--pal-highlight-light);
 		border-color: var(--pal-highlight-dark);
+		color: var(--pal-highlight-dark);
+	}
+	input::placeholder {
+		color: var(--pal-block-border);
+	}
+	input:hover::placeholder,
+	input:focus::placeholder {
+		color: var(--pal-highlight-dark);
 	}
 </style>
