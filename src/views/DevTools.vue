d11efee57d7dc8bd04cc3e972a2d3c64b61cc5c5
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-02-12 10:47:41 -0700

    Test update to watch actions

--- src/views/DevTools.vue
+++ src/views/DevTools.vue
@@ -2,7 +2,8 @@
 
 <template>
 	<div>
-		<h2>Some dope internal view that helps with development</h2>
+		<h2>Some dope internal view that helps with development.</h2>
+		<h3>This is where I keep things I don't want on my prod build.</h3>
 	</div>
 </template>
 
